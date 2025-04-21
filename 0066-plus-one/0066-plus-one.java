afa492f8b2c2208a5addc64433704d998b1c82de
Prasanth-Newbie <prasanthpalanisamy5236@gmail.com>
2025-04-21 13:45:42 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,7 +3,7 @@ class Solution {
         for(int i=digits.length-1;i>=0;i--)
         {
             if(digits[i]<9){
-            ++digits[i];
+            digits[i]++;
             return digits;
         }
         digits[i]=0;
